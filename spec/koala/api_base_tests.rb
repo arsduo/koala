2907508ec219b5dade49cd8ec8955ed6c54d6656
cbaclig <chris.baclig@gmail.com>
2010-06-03 09:43:36 -0700

    Added more tests for API base class

--- spec/koala/api_base_tests.rb
+++ spec/koala/api_base_tests.rb
@@ -29,9 +29,40 @@ class ApiBaseTests < Test::Unit::TestCase
       service.api('anything')
     end
     
-    it "should properly handle the http_component parameter"
+    it "should get the attribute of a Koala::Response given by the http_component parameter" do
+      http_component = :method_name
+      
+      response = mock('Mock KoalaResponse')
+      response.should_receive(http_component).and_return('')
+      response.stub(:body).and_return('')
+      
+      Koala.stub(:make_request).and_return(response)
+      
+      @service.api('anything', 'get', {}, :http_component => http_component)
+    end
     
-    it "should execute a block to test for errors if passed one"
+    it "should return the body of the request as JSON if no http_component is given" do
+      response = stub('response', :body => 'body')
+      Koala.stub(:make_request).and_return(response)
+      
+      json_body = mock('JSON body')
+      JSON.stub(:parse).and_return([json_body])
+      
+      @service.api('anything').should == json_body
+    end
+    
+    it "should execute a block with the response body if passed one" do
+      body = '{}'
+      Koala.stub(:make_request).and_return(Koala::Response.new(200, body, {}))
+      
+      yield_test = mock('Yield Tester')
+      yield_test.should_receive(:pass)
+      
+      @service.api('anything') do |arg| 
+        yield_test.pass
+        arg.should == JSON.parse(body)
+      end
+    end
     
     it "should handle rogue true/false as responses" do
       Koala.should_receive(:make_request).and_return(Koala::Response.new(200, 'true', {}))
