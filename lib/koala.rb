2610f2d999e9a7d0f078768516597dcd44aa58c7
Waseem Ahmad <cougar2149@gmail.com>
2010-11-10 10:18:59 +0530

    make access_token accessible via oauth token

--- lib/koala.rb
+++ lib/koala.rb
@@ -44,6 +44,7 @@ module Koala
     # JavaScript SDK at http://github.com/facebook/connect-js/.
 
     class API
+      attr_accessor :access_token
       # initialize with an access token 
       def initialize(access_token = nil)
         @access_token = access_token
