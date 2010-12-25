50347d59e2b0941e9c6fbfeee11c26dd6736ebc1
Alex Koppel <alex+git@alexkoppel.com>
2010-12-25 13:22:58 +0100

    Expanded HTTP service tests: added Typhoeus test suite and expanded Net::HTTP test cases.

--- spec/koala_spec_helper.rb
+++ spec/koala_spec_helper.rb
@@ -5,7 +5,7 @@ if defined?(RUBY_VERSION) && RUBY_VERSION =~ /1\.9/
   Rspec.configure do |c|
     c.mock_with :rspec
   end
-  
+
 else
   # Ruby 1.8.x
   require 'test/unit'
@@ -39,10 +39,14 @@ require 'koala/oauth/oauth_tests'
 # Subscriptions tests
 require 'koala/realtime_updates/realtime_updates_tests'
 
-# Services tests
-require 'koala/net_http_service_tests'
-
 # Test users tests
 require 'koala/test_users/test_users_tests'
 
+# Services tests
+require 'koala/net_http_service_tests'
+begin
+  require 'koala/typhoeus_service_tests'
+rescue LoadError
+  puts "Typhoeus tests will not be run because Typhoeus is not installed."
+end
 
