8755d982aefa960bb2fa790fc52ed06acd5fd059
Alex Koppel <alex+git@alexkoppel.com>
2011-01-17 16:58:27 +0100

    Fixed the merge.
    This reverts commit b2d179c2bb36b0dc2b46330cde5a5fbebe83113a.

--- spec/koala/graph_and_rest_api/graph_and_rest_api_with_token_tests.rb
+++ spec/koala/graph_and_rest_api/graph_and_rest_api_with_token_tests.rb
@@ -1,11 +1,16 @@
 class GraphAndRestAPIWithTokenTests < Test::Unit::TestCase
   describe "Koala GraphAndRestAPI without an access token" do
     include LiveTestingDataHelper
-    it_should_behave_like "Koala RestAPI with an access token"
-    it_should_behave_like "Koala GraphAPI with an access token"
-    
+
     before(:each) do
       @api = Koala::Facebook::GraphAndRestAPI.new(@token)
     end
+
+    it_should_behave_like "Koala RestAPI"
+    it_should_behave_like "Koala RestAPI with an access token"
+
+    it_should_behave_like "Koala GraphAPI"
+    it_should_behave_like "Koala GraphAPI with an access token"
+    it_should_behave_like "Koala GraphAPI with GraphCollection"
   end
 end
