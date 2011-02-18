627273c6bd3a9e88d2ea90b1ae4b6b1317acdc24
Alex Koppel <alex+git@alexkoppel.com>
2011-02-18 17:29:48 +0100

    Reorganized GraphAPI file to group functions by subject (objects, connections, likes, pictures, etc.).  Added put/delete for connections.  Updated documentation, including what to use for subjects that don't have all types of convenience method.

--- lib/koala/graph_api.rb
+++ lib/koala/graph_api.rb
@@ -5,8 +5,8 @@ module Koala
     module GraphAPIMethods
       # A client for the Facebook Graph API.
       # 
-      # See http://developers.facebook.com/docs/api for complete documentation
-      # for the API.
+      # See http://github.com/arsduo/koala for Ruby/Koala documentation
+      # and http://developers.facebook.com/docs/api for Facebook API documentation
       # 
       # The Graph API is made up of the objects in Facebook (e.g., people, pages,
       # events, photos) and the connections between them (e.g., friends,
@@ -23,13 +23,14 @@ module Koala
       # by the API at http://developers.facebook.com/docs/reference/api/.
       # 
       # You can obtain an access token via OAuth or by using the Facebook
-      # JavaScript SDK. See http://developers.facebook.com/docs/authentication/
-      # for details.
+      # JavaScript SDK. See the Koala and Facebook documentation for more information.
       # 
       # If you are using the JavaScript SDK, you can use the
       # Koala::Facebook::OAuth.get_user_from_cookie() method below to get the OAuth access token
       # for the active user from the cookie saved by the SDK.
-            
+         
+      # Objects
+
       def get_object(id, args = {})
         # Fetchs the given object from the graph.
         graph_call(id, args)
@@ -42,102 +43,122 @@ module Koala
         graph_call("", args.merge("ids" => ids.join(",")))
       end
       
-      def get_page(params)
-        result = graph_call(*params)
-        result ? GraphCollection.new(result, self) : nil # when facebook is down nil can be returned
-      end
+      def put_object(parent_object, connection_name, args = {})
+        # Writes the given object to the graph, connected to the given parent.
+        # See http://developers.facebook.com/docs/api#publishing for all of
+        # the supported writeable objects.
+        # 
+        # For example,
+        #     graph.put_object("me", "feed", :message => "Hello, world")
+        # writes "Hello, world" to the active user's wall.
+        #
+        # Most write operations require extended permissions. For example,
+        # publishing wall posts requires the "publish_stream" permission. See
+        # http://developers.facebook.com/docs/authentication/ for details about
+        # extended permissions.
     
+        raise APIError.new({"type" => "KoalaMissingAccessToken", "message" => "Write operations require an access token"}) unless @access_token
+        graph_call("#{parent_object}/#{connection_name}", args, "post")
+      end
+      
+      def delete_object(id)
+        # Deletes the object with the given ID from the graph.
+        raise APIError.new({"type" => "KoalaMissingAccessToken", "message" => "Delete requires an access token"}) unless @access_token
+        graph_call(id, {}, "delete")
+      end
+      
+      # Connections
+          
       def get_connections(id, connection_name, args = {})
         # Fetchs the connections for given object.
         result = graph_call("#{id}/#{connection_name}", args)
         result ? GraphCollection.new(result, self) : nil # when facebook is down nil can be returned
       end
-      
+
+      def put_connections(id, connection_name, args = {})
+        # Posts a certain connection
+        raise APIError.new({"type" => "KoalaMissingAccessToken", "message" => "Write operations require an access token"}) unless @access_token
+        graph_call("#{id}/#{connection_name}", args, "post")
+      end
+
+      def delete_connections(id, connection_name, args = {})
+        # Deletes a given connection
+        raise APIError.new({"type" => "KoalaMissingAccessToken", "message" => "Delete requires an access token"}) unless @access_token
+        graph_call("#{id}/#{connection_name}", args, "delete")
+      end
+
+      # Pictures
+      # to delete pictures, use delete_object(photo_id)
+      # note: you'll need the user_photos permission to actually access photos after uploading them 
     
       def get_picture(object, args = {})
+        # Gets a picture object, returning the URL (which Facebook sends as a header)
         result = graph_call("#{object}/picture", args, "get", :http_component => :headers)
         result["Location"]
       end    
-      
-      def put_object(parent_object, connection_name, args = {})
-        # Writes the given object to the graph, connected to the given parent.
-        # 
-        # For example,
-        # 
-        #     graph.put_object("me", "feed", :message => "Hello, world")
-        # 
-        # writes "Hello, world" to the active user's wall. Likewise, this
-        # will comment on a the first post of the active user's feed:
-        # 
-        #     feed = graph.get_connections("me", "feed")
-        #     post = feed["data"][0]
-        #     graph.put_object(post["id"], "comments", :message => "First!")
-        # 
-        # See http://developers.facebook.com/docs/api#publishing for all of
-        # the supported writeable objects.
-        # 
-        # Most write operations require extended permissions. For example,
-        # publishing wall posts requires the "publish_stream" permission. See
-        # http://developers.facebook.com/docs/authentication/ for details about
-        # extended permissions.
-
-        raise APIError.new({"type" => "KoalaMissingAccessToken", "message" => "Write operations require an access token"}) unless @access_token
-        graph_call("#{parent_object}/#{connection_name}", args, "post")
-      end
     
-      def put_wall_post(message, attachment = {}, profile_id = "me")
-        # Writes a wall post to the given profile's wall.
-        # 
-        # We default to writing to the authenticated user's wall if no
-        # profile_id is specified.  
-        # 
-        # attachment adds a structured attachment to the status message being
-        # posted to the Wall. It should be a dictionary of the form:
-        # 
-        #     {"name": "Link name"
-        #      "link": "http://www.example.com/",
-        #      "caption": "{*actor*} posted a new review",
-        #      "description": "This is a longer description of the attachment",
-        #      "picture": "http://www.example.com/thumbnail.jpg"}
-
-        self.put_object(profile_id, "feed", attachment.merge({:message => message}))
-      end
-      
       def put_picture(file_hash, args = {}, target_id = "me")
+        # Uploads a picture from a file hash
         raise APIError.new("type" => "KoalaInvaliFileFormat", "message" => "put_picture requires a hash describing the File to upload (see HTTPService::is_valid_file_hash?") unless file_hash.kind_of?(Hash)
         
         args["source"] = file_hash
         self.put_object(target_id, "photos", args)
       end
+    
+      # Wall posts
+      # To get wall posts, use get_connections(user, "feed")
+      # To delete a wall post, just use delete_object(post_id)
+    
+      def put_wall_post(message, attachment = {}, profile_id = "me")
+        # attachment is a hash describing the wall post
+        # (see X for more details)
+        # For instance, 
+        # 
+        #     {"name" => "Link name"
+        #      "link" => "http://www.example.com/",
+        #      "caption" => "{*actor*} posted a new review",
+        #      "description" => "This is a longer description of the attachment",
+        #      "picture" => "http://www.example.com/thumbnail.jpg"}
+
+        self.put_object(profile_id, "feed", attachment.merge({:message => message}))
+      end
+      
+      # Comments
+      # to delete comments, use delete_object(comment_id)
+      # to get comments, use get_connections(object, "likes")
       
       def put_comment(object_id, message)
         # Writes the given comment on the given post.
         self.put_object(object_id, "comments", {:message => message})
       end
-    
+        
+      # Likes
+      # to get likes, use get_connections(user, "likes")
+      
       def put_like(object_id)
         # Likes the given post.
         self.put_object(object_id, "likes")
       end
 
       def delete_like(object_id)
+        # Unlikes a given object for the logged-in user
         raise APIError.new({"type" => "KoalaMissingAccessToken", "message" => "Unliking requires an access token"}) unless @access_token
         graph_call("#{object_id}/likes", {}, "delete")
       end
-    
-      def delete_object(id)
-        # Deletes the object with the given ID from the graph.
-        raise APIError.new({"type" => "KoalaMissingAccessToken", "message" => "Unliking requires an access token"}) unless @access_token
-        graph_call(id, {}, "delete")
-      end
-    
+
+      # Search
+      
       def search(search_terms, args = {})
-        # Searches for a given term
+        # Searches for a given term among posts visible to the current user (or public posts if no token)
         result = graph_call("search", args.merge({:q => search_terms}))
         result ? GraphCollection.new(result, self) : nil # when facebook is down nil can be returned
-      end
+      end      
+      
+      # API access
     
       def graph_call(*args)
+        # Direct access to the Facebook API
+        # see any of the above methods for example invocations
         response = api(*args) do |response|
           # check for Graph API-specific errors
           if response.is_a?(Hash) && error_details = response["error"]
@@ -146,7 +167,17 @@ module Koala
         end
       
         response
+      end 
+      
+      # GraphCollection support
+      
+      def get_page(params)
+        # Pages through a set of results stored in a GraphCollection
+        # Used for connections and search results
+        result = graph_call(*params)
+        result ? GraphCollection.new(result, self) : nil # when facebook is down nil can be returned
       end
+      
     end
     
     
