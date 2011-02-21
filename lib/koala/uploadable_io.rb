105d0cd0a817e2ccfa48a2d72828657864742736
cbaclig <chris.baclig@gmail.com>
2011-02-20 17:13:37 -0800

    Added support for accepting a single Rails 3 POST file parameter

--- lib/koala/uploadable_io.rb
+++ lib/koala/uploadable_io.rb
@@ -2,17 +2,39 @@ require 'koala'
 
 module Koala
   class UploadableIO
-    def initialize(io_or_path, content_type)
-      if io_or_path.respond_to?(:read) or io_or_path.kind_of?(String)
-        @io_or_path = io_or_path
+    def initialize(io_or_path_or_mixed, content_type = nil)
+      if content_type.nil?
+        parse_init_mixed_param io_or_path_or_mixed
+      elsif !content_type.nil? && (io_or_path_or_mixed.respond_to?(:read) or io_or_path_or_mixed.kind_of?(String))
+        @io_or_path = io_or_path_or_mixed
         @content_type = content_type
       else
         raise KoalaError.new("Invalid arguments to initialize an UploadableIO")
       end
     end
-
+    
     def to_upload_io
       UploadIO.new(@io_or_path, @content_type, "koala-io-file.dum")
     end
+    
+    private
+      PARSE_STRATEGIES = [
+        :parse_rails_3_post_param
+      ]
+      
+      def parse_init_mixed_param(mixed)
+        PARSE_STRATEGIES.each do |method|
+          return if send(method, mixed) 
+        end
+      end
+      
+      # Expects a parameter of type ActionDispatch::Http::UploadedFile
+      def parse_rails_3_post_param(uploaded_file)
+        if uploaded_file.respond_to?(:content_type) and uploaded_file.respond_to?(:tempfile) and uploaded_file.tempfile.respond_to?(:path)
+          
+          @io_or_path = uploaded_file.tempfile.path
+          @content_type = uploaded_file.content_type
+        end
+      end
   end
 end
--- spec/koala/uploadable_io/uploadable_io_tests.rb
+++ spec/koala/uploadable_io/uploadable_io_tests.rb
@@ -25,7 +25,12 @@ class UploadableIOTests < Test::Unit::TestCase
         ]
         
         lambda { UploadableIO.new(*params) }.should_not raise_exception(KoalaError)
-      end      
+      end
+      
+      it "should accept a single Hash argument" do
+        lambda { UploadableIO.new({}) }.should_not raise_exception(Exception)
+        lambda { UploadableIO.new({}) }.should_not raise_exception(KoalaError)
+      end 
     end
     
     describe "getting an UploadIO" do
@@ -85,6 +90,40 @@ class UploadableIOTests < Test::Unit::TestCase
           UploadIO.should_receive(:new).with(anything, @koala_io_params[1], anything).and_return(@upload_io)
           
           UploadableIO.new(*@koala_io_params).to_upload_io.should == @upload_io          
+        end
+      end
+      
+      describe "when given a Rails 3 ActionDispatch::Http::UploadedFile" do
+        before(:each) do
+          @tempfile = stub('Tempfile', :path => true)
+          @uploaded_file = stub('ActionDispatch::Http::UploadedFile', 
+            :content_type => true,
+            :tempfile => @tempfile
+          )
+
+          @uploaded_file.stub!(:respond_to?).with(:content_type).and_return(true)
+          @uploaded_file.stub!(:respond_to?).with(:tempfile).and_return(@tempfile)
+          @tempfile.stub!(:respond_to?).with(:path).and_return(true)          
+        end
+        
+        it "should get the content type via the content_type method" do
+          upload_io = stub('UploadIO')
+          expected_content_type = stub('Content Type')
+          @uploaded_file.should_receive(:content_type).and_return(expected_content_type)
+
+          UploadIO.should_receive(:new).with(anything, expected_content_type, anything).and_return(upload_io)
+          
+          UploadableIO.new(@uploaded_file).to_upload_io.should == upload_io
+        end
+        
+        it "should get the path from the tempfile associated with the UploadedFile" do
+          upload_io = stub('UploadIO')
+          expected_path = stub('Tempfile')
+          @tempfile.should_receive(:path).and_return(expected_path)
+
+          UploadIO.should_receive(:new).with(expected_path, anything, anything).and_return(upload_io)
+          
+          UploadableIO.new(@uploaded_file).to_upload_io.should == upload_io          
         end
       end
     end
