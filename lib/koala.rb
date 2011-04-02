dd82ec37b1cf90453658dea96de5bf09dddf04e5
James Cox <james@imaj.es>
2011-04-02 17:18:37 -0400

    white space fixes

--- lib/koala.rb
+++ lib/koala.rb
@@ -52,7 +52,7 @@ module Koala
     # JavaScript SDK at http://github.com/facebook/connect-js/.
 
     class API
-      # initialize with an access token 
+      # initialize with an access token
       def initialize(access_token = nil)
         @access_token = access_token
       end
@@ -211,7 +211,7 @@ module Koala
           string = info["access_token"]
         end
       end
-    
+
       # provided directly by Facebook
       # see https://github.com/facebook/crypto-request-examples/blob/master/sample.rb
       # and http://developers.facebook.com/docs/authentication/canvas/encryption_proposal
@@ -313,7 +313,7 @@ module Koala
       end
     end
   end
-  
+
   class KoalaError< StandardError; end
 
   # finally, set up the http service Koala methods used to make requests
