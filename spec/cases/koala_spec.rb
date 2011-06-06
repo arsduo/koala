db49f2d5b5d4dc0049ef49650cade5776958378a
Alex Koppel <alex+git@alexkoppel.com>
2011-06-06 15:33:46 +0200

    Fixed bug that broke live tests by setting http_service = MockHTTPService midway through.

--- spec/cases/koala_spec.rb
+++ spec/cases/koala_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe "Koala" do
+  it "has an http_service accessor" do
+    Koala.respond_to?(:http_service)
+  end
 
   it "should let an http service be set" do
+    current_service = Koala.http_service
     Koala.http_service = Koala::MockHTTPService
     Koala.http_service.should == Koala::MockHTTPService
+    # reset the service back to the original one (important for live tests)
+    Koala.http_service = current_service
+  end
+
+  it "sets Net::HTTP as the base service" do
     Koala.base_http_service.should == Koala::NetHTTPService
   end
 
@@ -17,11 +26,15 @@ describe "Koala" do
     end
   end
 
-
   describe ".make_request" do
-
+    
     before :each do
+      @old_service = Koala.http_service
       Koala.http_service = Koala::MockHTTPService
+    end
+    
+    after :each do
+      Koala.http_service = @old_service
     end
 
     it "should use SSL if Koala.always_use_ssl is set to true, even if there's no token" do
