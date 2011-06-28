3c026321334a6eb080390ed34b9e957ab44e38cf
Aaron Spiegel <spiegela@gmail.com>
2011-06-27 23:01:33 -0500

    Fixes for JRuby-HttpClientService

--- lib/koala/http_services/http_client_service.rb
+++ lib/koala/http_services/http_client_service.rb
@@ -9,18 +9,20 @@ module Koala
       # you can pass arguments directly to HttpClient using the :client_options key
       client_options = {:params => args}.merge(options[:client_options] || {})
 
+      scheme = (args["access_token"] || @always_use_ssl || options[:use_ssl]) ? "https" : "http"
+      port   = scheme == "https" ? 443 : 80
+
       # if proxy/timeout options aren't passed, check if defaults are set
       client_options[:default_proxy] ||= proxy
       client_options[:so_timeout]    ||= timeout
+      client_options[:disable_response_handler] = true
       
-      prefix = (args["access_token"] || @always_use_ssl || options[:use_ssl]) ? "https" : "http"
-
-      client   = HTTP::Client.new(server(options), client_options)
+      client   = HTTP::Client.new(client_options.merge(:host => server(options), :port => port, :scheme => scheme))
+      response = client.send(verb, path, args)
       
-      response = client.send(verb, "#{prefix}://#{server(options)}#{path}")
       status_code  = response.get_status_line.get_status_code
       headers_hash = Hash[ response.get_all_headers.map{|h| [h.get_name, h.get_value]} ]
-      body         = EntityUtils.to_string response.get_entity
+      body         = EntityUtils.to_string(response.get_entity)
       Koala::Response.new(status_code, body, headers_hash)
     end
   
