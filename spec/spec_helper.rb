ed94255d7c408a26f8e04e62591c78fe72565742
Chris Baclig <chris.baclig@gmail.com>
2011-08-08 09:50:45 -0700

    Closes gh-84. Downgrading to syck parser when running the test suite for Ruby versions that rely on a buggy, bundled version of the Psych YAML parser.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,23 @@ rescue LoadError
   puts 'Although not required, bundler is recommended for running the tests.'
 end
 
+# In Ruby 1.9.2 versions before patchlevel 290, the default Psych
+# parser has an issue with YAML merge keys, which 
+# fixtures/mock_facebook_responses.yml relies heavily on.
+# 
+# Anyone using an earlier version will see missing mock response
+# errors when running the test suite similar to this:
+# 
+# RuntimeError:
+#   Missing a mock response for graph_api: /me/videos: source=[FILE]: post: with_token
+#   API PATH: /me/videos?source=[FILE]&format=json&access_token=*
+# 
+# For now, it seems the best fix is to just downgrade to the old syck YAML parser
+# for these troubled versions.
+#
+# See https://github.com/tenderlove/psych/issues/8 for more details
+YAML::ENGINE.yamler = 'syck' if RUBY_VERSION == '1.9.2' && RUBY_PATCHLEVEL < 290
+
 # load the library
 require 'koala'
 
