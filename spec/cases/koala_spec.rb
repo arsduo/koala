9e6de3dde8e1c3949990c367dc821bd508025bcf
Chris Baclig <chris.baclig@gmail.com>
2011-09-11 23:35:12 -0700

    Fixed undefined method in koala_spec for testing Koala.make_request

--- spec/cases/koala_spec.rb
+++ spec/cases/koala_spec.rb
@@ -35,14 +35,14 @@ describe "Koala" do
     end
   end
 
-  define "make_request" do
+  describe "make_request" do
     it "passes all its arguments to the http_service" do
-      http_service = stub("http_service")
       path = "foo"
       args = {:a => 2}
       verb = "get"
       options = {:c => :d}
-      http_service.should_receive(:make_request).with(path, args, verb, options)
+      
+      Koala.http_service.should_receive(:make_request).with(path, args, verb, options)
       Koala.make_request(path, args, verb, options)
     end
   end
