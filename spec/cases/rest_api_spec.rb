46d3ecca1f8e09cd04abe7055da943877a30f5c3
Alex Koppel <alex+git@alexkoppel.com>
2011-09-30 16:05:24 +0200

    Removed duplicate tests from RestAPI spec (they're run in the API spec).

--- spec/cases/rest_api_spec.rb
+++ spec/cases/rest_api_spec.rb
@@ -19,23 +19,4 @@ describe "Koala::Facebook::RestAPI" do
       api = Koala::Facebook::RestAPI.new("token")
     end
   end
-
-  context "without an access token" do
-    before :each do
-      @api = Koala::Facebook::API.new
-    end
-
-    it_should_behave_like "Koala RestAPI"
-    it_should_behave_like "Koala RestAPI without an access token"
-  end
-
-  context "with an access token" do
-    before :each do
-      @api = Koala::Facebook::API.new(@token)
-    end
-
-    it_should_behave_like "Koala RestAPI"
-    it_should_behave_like "Koala RestAPI with an access token"
-  end
-
 end
