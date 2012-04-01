f3e2d021012ba2ce3db3a150e0553893b9c3ade2
Alex Koppel <alex+git@alexkoppel.com>
2012-04-01 15:04:18 +0200

    Update RDOC info for the new logger

--- lib/koala/utils.rb
+++ lib/koala/utils.rb
@@ -1,8 +1,7 @@
 module Koala
   module Utils
-    
-    # Add a logger
-    # Uses the ruby logger as default
+
+    # Utility methods used by Koala.
     require 'logger'
     require 'forwardable'
 
@@ -11,14 +10,16 @@ module Koala
 
     def_delegators :logger, :debug, :info, :warn, :error, :fatal, :level, :level=
 
-    attr_accessor :logger 
+    # The Koala logger, an instance of the standard Ruby logger, pointing to STDOUT by default.
+    # In Rails projects, you can set this to Rails.logger.
+    attr_accessor :logger
     self.logger = Logger.new(STDOUT)
     self.logger.level = Logger::ERROR
-    
+
     # @private
     DEPRECATION_PREFIX = "KOALA: Deprecation warning: "
 
-    # Prints a deprecation message.  
+    # Prints a deprecation message.
     # Each individual message will only be printed once to avoid spamming.
     def deprecate(message)
       @posted_deprecations ||= []
