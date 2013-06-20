7c5a539d961ec2d5e1aaec2c2f443ccddde71cc1
Louis Simoneau <simoneau.louis@gmail.com>
2013-06-20 10:21:12 +1000

    wrap hash.select in Hash[] to return a hash in ruby 1.8.7

--- lib/koala/http_service.rb
+++ lib/koala/http_service.rb
@@ -219,7 +219,7 @@ module Koala
 
     def self.faraday_options(options)
       valid_options = [:request, :proxy, :ssl, :builder, :url, :parallel_manager, :params, :headers, :builder_class]
-      options.select { |key,value| valid_options.include?(key) }
+      Hash[ options.select { |key,value| valid_options.include?(key) } ]
     end
   end
 
