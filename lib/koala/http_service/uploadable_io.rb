f5453b4521b222565904b8d584e2e5871eba8a6e
Yannick Schutz <yannick.schutz@gmail.com>
2013-07-16 16:27:17 +0200

    Accepts Tempfile as File objects in UploadableIO

--- lib/koala/http_service/uploadable_io.rb
+++ lib/koala/http_service/uploadable_io.rb
@@ -1,4 +1,5 @@
 require "net/http/post/multipart"
+require "tempfile"
 
 module Koala
   module HTTPService
@@ -80,7 +81,7 @@ module Koala
 
       # takes a file object
       def self.file_param?(file)
-        file.kind_of?(File)
+        file.kind_of?(File) || file.kind_of?(Tempfile)
       end
 
       def parse_file_object(file, content_type = nil)
@@ -180,7 +181,7 @@ module Koala
       end
     end
   end
-  
+
   # @private
   # legacy support for when UploadableIO lived directly under Koala
   UploadableIO = HTTPService::UploadableIO
