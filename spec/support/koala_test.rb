362b77084027d62eb0d7c1418c3e50a83c5fdbd3
Alex Koppel <alex@alexkoppel.com>
2013-10-26 13:14:52 -0500

    Fix intermittent bug

--- spec/support/koala_test.rb
+++ spec/support/koala_test.rb
@@ -56,12 +56,12 @@ module KoalaTest
       config.before :each do
         @token = KoalaTest.oauth_token
         Koala::Utils.stub(:deprecate) # never fire deprecation warnings
+
+        # Clean up Koala config
+        Koala.reset_config
       end
 
       config.after :each do
-        # Clean up Koala config
-        Koala.reset_config
-
         # if we're working with a real user, clean up any objects posted to Facebook
         # no need to do so for test users, since they get deleted at the end
         if @temporary_object_id && KoalaTest.real_user?
