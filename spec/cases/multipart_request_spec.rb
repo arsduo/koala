6822ae0e0fd12cc1ec18899f4a68a478da1ae7c7
Alex Koppel <alex@alexkoppel.com>
2014-02-01 12:01:25 -0600

    Fix MultipartRequest spec

--- spec/cases/multipart_request_spec.rb
+++ spec/cases/multipart_request_spec.rb
@@ -4,18 +4,18 @@ describe Koala::HTTPService::MultipartRequest do
   it "is a subclass of Faraday::Request::Multipart" do
     Koala::HTTPService::MultipartRequest.superclass.should == Faraday::Request::Multipart
   end
-  
+
   it "defines mime_type as multipart/form-data" do
     Koala::HTTPService::MultipartRequest.mime_type.should == 'multipart/form-data'
   end
-  
+
   describe "#process_request?" do
     before :each do
-      @env = {}
+      @env = Faraday::Env.new
       @multipart = Koala::HTTPService::MultipartRequest.new
       @multipart.stub(:request_type).and_return("")
     end
-    
+
     # no way to test the call to super, unfortunately
     it "returns true if env[:body] is a hash with at least one hash in its values" do
       @env[:body] = {:a => {:c => 2}}
@@ -40,21 +40,21 @@ describe Koala::HTTPService::MultipartRequest do
     it "returns false if env[:body] is a hash without an array or hash value" do
       @env[:body] = {:a => 3}
       @multipart.process_request?(@env).should be_false
-    end    
+    end
   end
-  
+
   describe "#process_params" do
     before :each do
       @parent = Faraday::Request::Multipart.new
-      @multipart = Koala::HTTPService::MultipartRequest.new 
-      @block = lambda {|k, v| "#{k}=#{v}"}     
+      @multipart = Koala::HTTPService::MultipartRequest.new
+      @block = lambda {|k, v| "#{k}=#{v}"}
     end
-    
+
     it "is identical to the parent for requests without a prefix" do
       hash = {:a => 2, :c => "3"}
       @multipart.process_params(hash, &@block).should == @parent.process_params(hash, &@block)
     end
-    
+
     it "replaces encodes [ and ] if the request has a prefix" do
       hash = {:a => 2, :c => "3"}
       prefix = "foo"
@@ -62,5 +62,4 @@ describe Koala::HTTPService::MultipartRequest do
       @multipart.process_params(hash, prefix, &@block).join("&").should == @parent.process_params(hash, prefix, &@block).join("&").gsub(/\[/, "%5B").gsub(/\]/, "%5D")
     end
   end
-  
 end
