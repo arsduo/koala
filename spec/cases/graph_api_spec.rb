75f183f2540cb92686c8b7b0fb9d67d9d50379b7
Alex Koppel <alex@alexkoppel.com>
2017-02-18 10:26:47 -0600

    Update tests based on failures

--- spec/cases/graph_api_spec.rb
+++ spec/cases/graph_api_spec.rb
@@ -31,7 +31,7 @@ describe 'Koala::Facebook::GraphAPIMethods' do
     context '#get_picture' do
       it 'returns result of block' do
         allow(@api).to receive(:api).and_return({"data" => {"is_silhouette" => false, "url" => result}})
-        expect(@api.get_picture('lukeshepard', &post_processing)["result"]).to eq(result)
+        expect(@api.get_picture('koppel', &post_processing)["result"]).to eq(result)
       end
     end
 
